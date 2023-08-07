19b9784582f0b1bb2f34506d0a863076da7d6d49
sayedulhoque44 <sayedulhoque3544@gmail.com>
2023-08-07 23:42:08 +0600

    l

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-// _____________________Mongodb_________________________
+// //_____________________Mongodb_________________________
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrxxnjj.mongodb.net/?retryWrites=true&w=majority`;
 
